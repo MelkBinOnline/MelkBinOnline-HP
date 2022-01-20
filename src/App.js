58e7fa197d24ed0e8dbf0431fde1283889ab2ac7
sky1991 <sky.dfo@gmail.com>
2022-01-20 12:52:59 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          test for start
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className='App-link' href="https://ian-0000.hostman.site/" target="_blank" aria-label='link'>ian0000</a>
       </header>
     </div>
   );
