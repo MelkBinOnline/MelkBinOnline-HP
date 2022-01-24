7104004d0fa9175d94dde4d8aefa4056af0075ee
sky1991 <sky.dfo@gmail.com>
2022-01-24 18:57:41 +0330

    tryToFix

--- src/caseComponent/case2.js
+++ src/caseComponent/case2.js
@@ -5,7 +5,7 @@ const Case2 = () => {
   return (
   <div className='case'>
       <div className="img-con">
-          <img  src={srcImg} alt="" />
+          <img  src={srcImg} alt="img" />
       </div>
       <a className='link' href="https://i-0004.hostman.site/">بازدید آنلاین</a>
   </div>
--- src/caseComponent/caseComponent.js
+++ src/caseComponent/caseComponent.js
@@ -5,7 +5,7 @@ const CaseComponent = () => {
   return (
   <div className='case'>
       <div className="img-con">
-          <img  src={srcImg} alt="" />
+          <img  src={srcImg} alt="img" />
       </div>
       <a className='link' href="https://i-0003.hostman.site/">بازدید آنلاین</a>
   </div>
