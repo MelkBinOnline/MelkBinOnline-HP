6c7718a495ecc160b1cfd170507ab6d1fc49336a
sky1991 <sky.dfo@gmail.com>
2022-01-31 14:59:07 +0330

    someChanges

--- src/caseComponent/caseListComponent.js
+++ src/caseComponent/caseListComponent.js
@@ -9,14 +9,9 @@ import './caseListStyle.css'
 const data = {
   case : [
     {
-      id : 'i_000',
-      cover: 'https://test-m-page2.hostman.site/covers/cover-i-000.jpg',
-      url: "https://test-m-page2.hostman.site/i-0/i-0.htm"
-    },
-    {
-      id : 'i_001',
-      cover: 'https://test-m-page2.hostman.site/covers/cover-i-001.jpg',
-      url: "https://test-m-page2.hostman.site/i-1/i-1.htm"
+      id : 'i_003',
+      cover: 'https://test-m-page2.hostman.site/covers/cover-i-003.jpg',
+      url: "https://test-m-page2.hostman.site/i-3/i-3.htm"
     },
     {
       id : 'i_002',
@@ -24,9 +19,14 @@ const data = {
       url: "https://test-m-page2.hostman.site/i-2/i-2.htm"
     },
     {
-      id : 'i_003',
-      cover: 'https://test-m-page2.hostman.site/covers/cover-i-003.jpg',
-      url: "https://test-m-page2.hostman.site/i-3/i-3.htm"
+      id : 'i_001',
+      cover: 'https://test-m-page2.hostman.site/covers/cover-i-001.jpg',
+      url: "https://test-m-page2.hostman.site/i-1/i-1.htm"
+    },
+    {
+      id : 'i_000',
+      cover: 'https://test-m-page2.hostman.site/covers/cover-i-000.jpg',
+      url: "https://test-m-page2.hostman.site/i-0/i-0.htm"
     }
   ]
 }
